43ed08adbd442ad38ca957468e4534837fed7739
Taro Yamashita <taro.ymst@gmail.com>
2017-06-09 07:26:42 +0900

    Convert between hiragana and katakana (only full-width).

---
+++ src/convert.rs
@@ -0,0 +1,69 @@
+//! Functions which convert strings.
+use std::collections::HashMap;
+use std::vec::Vec;
+
+use conv_option::ConvOption;
+use conv_table::{ConvTable, HiraKana, exclude_table};
+
+/// Convert strings with conversion table
+fn convert(text: &str, table: HashMap<u32, String>) -> String {
+    let mut converted = Vec::new();
+
+    for c in text.chars() {
+        let ord = c as u32;
+        match table.get(&ord) {
+            Some(s) => converted.push(s.to_string()),
+            None => converted.push(c.to_string()),
+        }
+    }
+
+    converted.into_iter().collect::<String>()
+}
+
+/// Convert from hiragana to full-witdh katakana
+pub fn hira2kata(text: &str, option: ConvOption) -> String {
+    let table = match option.ignore_chars() {
+        Some(chars) => exclude_table(chars, HiraKana::HiraToKana),
+        None => HiraKana::HiraToKana.table(),
+    };
+    convert(text, table)
+}
+
+/// Convert from full-width katakana to hiragana
+pub fn kata2hira(text: &str, option: ConvOption) -> String {
+    let table = match option.ignore_chars() {
+        Some(chars) => exclude_table(chars, HiraKana::KanaToHira),
+        None => HiraKana::KanaToHira.table(),
+    };
+    convert(text, table)
+}
+
+#[cfg(test)]
+mod tests {
+    use conv_option::ConvOptionBuilder;
+    use super::*;
+
+    #[test]
+    fn test_hira2kata() {
+        let option = ConvOptionBuilder::new().build();
+        assert_eq!(hira2kata("あいうえお", option), "アイウエオ");
+    }
+
+    #[test]
+    fn test_hira2kata_with_ignore() {
+        let option = ConvOptionBuilder::new().ignore("きくけ").build();
+        assert_eq!(hira2kata("かきくけこ", option), "カきくけコ");
+    }
+
+    #[test]
+    fn test_kata2hira() {
+        let option = ConvOptionBuilder::new().build();
+        assert_eq!(kata2hira("アイウエオ", option), "あいうえお");
+    }
+
+    #[test]
+    fn test_kata2hira_with_ignore() {
+        let option = ConvOptionBuilder::new().ignore("サソ").build();
+        assert_eq!(kata2hira("サシスセソ", option), "サしすせソ");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 mod conv_option;
 mod conv_table;
+mod convert;
+
+pub use convert::hira2kata;
+pub use convert::kata2hira;
