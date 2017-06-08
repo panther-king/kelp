22baab7b83b65cf9d84b567751265cb1b6c4887d
Taro Yamashita <taro.ymst@gmail.com>
2017-06-09 07:31:33 +0900

    refactoring

--- src/convert.rs
+++ src/convert.rs
@@ -5,21 +5,6 @@ use std::vec::Vec;
 use conv_option::ConvOption;
 use conv_table::{ConvTable, HiraKana, exclude_table};
 
-/// Convert strings with conversion table
-fn convert(text: &str, table: HashMap<u32, String>) -> String {
-    let mut converted = Vec::new();
-
-    for c in text.chars() {
-        let ord = c as u32;
-        match table.get(&ord) {
-            Some(s) => converted.push(s.to_string()),
-            None => converted.push(c.to_string()),
-        }
-    }
-
-    converted.into_iter().collect::<String>()
-}
-
 /// Convert from hiragana to full-witdh katakana
 pub fn hira2kata(text: &str, option: ConvOption) -> String {
     let table = match option.ignore_chars() {
@@ -36,6 +21,21 @@ pub fn kata2hira(text: &str, option: ConvOption) -> String {
         None => HiraKana::KanaToHira.table(),
     };
     convert(text, table)
+}
+
+/// Convert strings with conversion table
+fn convert(text: &str, table: HashMap<u32, String>) -> String {
+    let mut converted = Vec::new();
+
+    for c in text.chars() {
+        let ord = c as u32;
+        match table.get(&ord) {
+            Some(s) => converted.push(s.to_string()),
+            None => converted.push(c.to_string()),
+        }
+    }
+
+    converted.into_iter().collect::<String>()
 }
 
 #[cfg(test)]
