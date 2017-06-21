1429724a72cab21b6ca0ec42fcaa783a37d6f021
Taro Yamashita <taro.ymst@gmail.com>
2017-06-22 08:00:45 +0900

    Re-definition functions.

--- src/convert.rs
+++ src/convert.rs
@@ -6,13 +6,41 @@ use conv_option::ConvOption;
 use conv_table::HiraKana;
 
 /// Convert from hiragana to full-witdh katakana
+///
+/// ```
+/// use kelp::ConvOptionBuilder;
+/// use kelp::hira2kata;
+///
+/// let option = ConvOptionBuilder::new().build();
+/// let converted = hira2kata("あいうえお", option);
+/// assert_eq!("アイウエオ", converted);
+///
+/// let option = ConvOptionBuilder::new().ignore("かこ").build();
+/// let converted = hira2kata("かきくけこ", option);
+/// assert_eq!("かキクケこ", converted);
+/// ```
 pub fn hira2kata(text: &str, option: ConvOption) -> String {
-    convert(text, HiraKana::HiraToKana.to_table(), option)
+    let table = HiraKana::HiraToKana.to_table();
+    convert(text, table, option)
 }
 
 /// Convert from full-width katakana to hiragana
+///
+/// ```
+/// use kelp::ConvOptionBuilder;
+/// use kelp::kata2hira;
+///
+/// let option = ConvOptionBuilder::new().build();
+/// let converted = kata2hira("アイウエオ", option);
+/// assert_eq!("あいうえお", converted);
+///
+/// let option = ConvOptionBuilder::new().ignore("キクケ").build();
+/// let converted = kata2hira("カキクケコ", option);
+/// assert_eq!("かキクケこ", converted);
+/// ```
 pub fn kata2hira(text: &str, option: ConvOption) -> String {
-    convert(text, HiraKana::KanaToHira.to_table(), option)
+    let table = HiraKana::KanaToHira.to_table();
+    convert(text, table, option)
 }
 
 /// Convert strings refers conversion table and option settings
@@ -36,27 +64,33 @@ mod tests {
     use conv_option::ConvOptionBuilder;
     use super::*;
 
-    #[test]
-    fn test_hira2kata() {
-        let option = ConvOptionBuilder::new().build();
-        assert_eq!(hira2kata("あいうえお", option), "アイウエオ");
+    fn hiragana() -> &'static str {
+        "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞ\
+         ただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼ\
+         ぽまみむめもゃやゅゆょよらりるれろわをんーゎゐゑゕゖゔゝゞ・\
+         「」。、"
+    }
+
+    fn full_kana() -> &'static str {
+        "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾ\
+         タダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボ\
+         ポマミムメモャヤュユョヨラリルレロワヲンーヮヰヱヵヶヴヽヾ・\
+         「」。、"
     }
 
     #[test]
-    fn test_hira2kata_with_ignore() {
-        let option = ConvOptionBuilder::new().ignore("きくけ").build();
-        assert_eq!(hira2kata("かきくけこ", option), "カきくけコ");
+    fn test_hira2kata() {
+        let before = hiragana();
+        let after = full_kana();
+        let option = ConvOptionBuilder::new().build();
+        assert_eq!(hira2kata(before, option), after);
     }
 
     #[test]
     fn test_kata2hira() {
+        let before = full_kana();
+        let after = hiragana();
         let option = ConvOptionBuilder::new().build();
-        assert_eq!(kata2hira("アイウエオ", option), "あいうえお");
-    }
-
-    #[test]
-    fn test_kata2hira_with_ignore() {
-        let option = ConvOptionBuilder::new().ignore("サソ").build();
-        assert_eq!(kata2hira("サシスセソ", option), "サしすせソ");
+        assert_eq!(kata2hira(before, option), after);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ mod conv_option;
 mod conv_table;
 mod convert;
 
+pub use conv_option::ConvOptionBuilder;
 pub use convert::hira2kata;
 pub use convert::kata2hira;
