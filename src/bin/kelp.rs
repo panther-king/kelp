2f1f9e2a97cf816c37d3e71832f158953e793b25
Taro Yamashita <taro.ymst@gmail.com>
2017-06-30 08:39:56 +0900

    Added for CLI.

---
+++ src/bin/kelp.rs
@@ -0,0 +1,63 @@
+extern crate clap;
+extern crate kelp;
+
+use clap::{App, Arg};
+use kelp::h2z;
+use kelp::hira2hkata;
+use kelp::hira2kata;
+use kelp::kata2hira;
+use kelp::z2h;
+use kelp::conv_option::ConvOption;
+
+fn main() {
+    let matches = App::new("kelp")
+        .version("0.1")
+        .author("Taro Yamashita <taro.ymst@gmail.com>")
+        .about("A conversion tool of Japanese")
+        .arg(Arg::with_name("INPUT")
+            .required(true)
+            .help("A text that you want to convert"))
+        .arg(Arg::with_name("convert")
+            .short("c")
+            .long("conv")
+            .takes_value(true)
+            .help("Specified conversion pattern"))
+        .arg(Arg::with_name("ascii")
+            .short("a")
+            .long("ascii")
+            .help("Convert with ascii if specified"))
+        .arg(Arg::with_name("digit")
+            .short("d")
+            .long("digit")
+            .help("Convert with digit if specified"))
+        .arg(Arg::with_name("kana")
+            .short("k")
+            .long("kana")
+            .help("Convert with kana if specified"))
+        .arg(Arg::with_name("ignore")
+            .short("i")
+            .long("ignore")
+            .takes_value(true)
+            .help("Specified ignore characters"))
+        .get_matches();
+
+    let option = ConvOption::build()
+        .ascii(matches.is_present("ascii"))
+        .digit(matches.is_present("digit"))
+        .kana(matches.is_present("digit"))
+        .ignore(matches.value_of("ignore").unwrap_or(""))
+        .finalize();
+
+    let text = matches.value_of("INPUT").unwrap();
+
+    let converted = match matches.value_of("convert") {
+        Some(s) if s == "h2z" => h2z(text, option),
+        Some(s) if s == "h2hk" => hira2hkata(text, option),
+        Some(s) if s == "h2k" => hira2kata(text, option),
+        Some(s) if s == "k2h" => kata2hira(text, option),
+        Some(s) if s == "z2h" => z2h(text, option),
+        _ => text.to_string(),
+    };
+
+    println!("{}", converted);
+}
